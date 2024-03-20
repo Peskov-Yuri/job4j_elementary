cdc38b586de738daea4d78aa0d401d27d3783251
Юрий <6grobik@mail.ru>
2024-03-20 19:41:39 +0300

    5.0.1. Сумма чисел. #226416 #486691

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -4,30 +4,18 @@ public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-            sum = sum + 2;
-        }
-        return sum;
-    }
-
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
+            sum += i;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(1, 10));
+        System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
-        System.out.println(sum(-1, -10));
-
-        System.out.println(sumByEven(1, 10));
-        System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1, 1));
-        System.out.println(sumByEven(-1, -10));
+        System.out.println(sum(4, 22));
+        System.out.println(sum(1, 9));
+        System.out.println(sum(3, 15));
+        System.out.println(sum(10, 2));
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,30 +7,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CounterTest {
 
     @Test
-    void whenSumEvenNumbersFromOneToTenThenThirty() {
-        int start = 1;
-        int finish = 10;
-        int result = Counter.sumByEven(start, finish);
-        int expected = 30;
+    void sumNumbers1() {
+        int expected = 55;
+        int result = Counter.sum(0, 10);
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenSumEvenMinusNumbersFromOneToTenThenThirty() {
-        int start = -1;
-        int finish = -10;
-        int result = Counter.sumByEven(start, finish);
+    void sumNumbers2() {
+        int expected = 33;
+        int result = Counter.sum(3, 8);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void sumNumbers3() {
+        int expected = 1;
+        int result = Counter.sum(1, 1);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void sumNumbers4() {
+        int expected = 247;
+        int result = Counter.sum(4, 22);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void sumNumbers5() {
+        int expected = 45;
+        int result = Counter.sum(1, 9);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void sumNumbers6() {
+        int expected = 117;
+        int result = Counter.sum(3, 15);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void sumNumbers7() {
         int expected = 0;
+        int result = Counter.sum(10, 2);
         assertThat(result).isEqualTo(expected);
     }
-
-    @Test
-    void whenSumEvenNumbersFromOneToTenThenThirty12() {
-        int start = 3;
-        int finish = 8;
-        int result = Counter.sumByEven(start, finish);
-        int expected = 18;
-        assertThat(result).isEqualTo(expected);
-    }
-
 }
